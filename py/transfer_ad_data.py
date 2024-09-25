1e2fcf12fd470395ad326559816a1231db5a8fd4
alberich <qingdou888@gmail.com>
2024-09-25 11:12:45 +0800

    能指定排序字段下标和目标字段下标

--- py/transfer_ad_data.py
+++ py/transfer_ad_data.py
@@ -2,14 +2,22 @@ import pandas as pd
 import sys
 import time
 
+
+# 排序字段下标由sys.argv[2]指定
+sort_col = int(sys.argv[2])
+
+# 目标字段下标由sys.argv[3]指定
+target_col = int(sys.argv[3])
+
+print(f"日期")
 # 读取CSV文件
 df = pd.read_csv(sys.argv[1])
 
 # 确保第一列是日期类型
-df.iloc[:, 0] = pd.to_datetime(df.iloc[:, 0])
+df.iloc[:, sort_col] = pd.to_datetime(df.iloc[:, sort_col])
 
 # 按照第一列日期从小到大排序
-df_sorted = df.sort_values(by=df.columns[0])
+df_sorted = df.sort_values(by=df.columns[sort_col])
 
 # 获取最后一列并转置
 last_column_transposed = df_sorted.iloc[:, -1].to_numpy().T
@@ -23,5 +31,5 @@ timestamp = int(time.time())
 output_filename = f"out_{timestamp}.csv"
 df_transposed.to_csv(output_filename, index=False, header=False)
 
-print(f"转置后的最后一列已保存到 {output_filename} 文件中。")
+#print(f"转置后的最后一列已保存到 {output_filename} 文件中。")
 print('\t'.join(map(str,last_column_transposed)))
