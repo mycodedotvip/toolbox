53420fd3163391c27e45621c09af30e4576a10d8
alberich <qingdou888@gmail.com>
2024-10-15 16:51:40 +0800

    修正了金额求和的bug

--- py/traverse_receipt_files.py
+++ py/traverse_receipt_files.py
@@ -12,11 +12,11 @@ def parse_args(argc, argv):
 
 def extract_parts(s):
     # 使用正则表达式提取前缀、中间部分和后缀
-    # wo_kao_3.1415.pdf
-    # prefix: wo_kao_
+    # wo_2_3.1415.pdf
+    # prefix: wo_2_
     # middle: 3.1415
     # suffix: .pdf
-    match = re.match(r'([\D]*)([\d.]+)(\.[^.]+)', s)
+    match = re.match(r'(\w*)_([\d.]+)(\.[^.]+)', s)
     if match:
         prefix = match.group(1)
         middle = match.group(2)
@@ -26,27 +26,45 @@ def extract_parts(s):
         return None, None, None
 
 
-if __name__ == "__main__":
-    path = parse_args(len(sys.argv), sys.argv)
-    if path is None:
-        exit(-1)
-
+def traverse_dir(root_path):
     total = 0.0
-    for root, _, files in os.walk(path):
-        total_root = 0.0
+    for r, dirs, files in os.walk(root_path):
+        for d in dirs:
+            if d.startswith('.'):
+                continue
+
+            sub_dir = os.path.join(root_path, d)
+            total_sum = traverse_dir(sub_dir)
+            total += total_sum
+            print(f"TOTAL_ROOT {total_sum} UNDER {os.path.basename(sub_dir)}")
+
+            # total_root = 0.0
         for filename in files:
             if filename.startswith('.'):
                 continue
 
             p, m, s = extract_parts(filename)
             if p is None or m is None or s is None:
-                print(f"NOT MATCH in DIR: {root}/{filename}")
+                # print(f"NOT MATCH in DIR: {root_path}/{filename}")
                 continue
-            print(f"HIT in DIR - {root}/{filename}\n\tprefix:{p}\n\tmiddle:{m}\n\tsuffix:{s}")
-            if m.isdigit():
+            # print(f"HIT in DIR - {root_path}/{filename}\n\tprefix:{p}\n\tmiddle:{m}\n\tsuffix:{s}")
+            try:
                 total += float(m)
-                total_root += float(m)
-        print(f"TOTAL_ROOT {root} - {total_root}")
-    print(f"TOTAL ALL {root} - {total}")
+                print(f"TOTAL_ROOT {float(m)} UNDER {os.path.join(os.path.basename(r), filename)}")
+
+            except ValueError:
+                print(f"ERROR: {m}")
+
+        return round(total, 2)
+
+    return round(total, 2)
+
+
+if __name__ == "__main__":
+    path = parse_args(len(sys.argv), sys.argv)
+    if path is None:
+        exit(-1)
+
+    print(f"TOTAL {traverse_dir(path)}")
     print("TASK DONE!")
     exit(0)
